f2d5ef7b09b8ddf7407f835bb0f91c0fcf377ef3
Yonas W <yonaseth12@gmail.com>
2025-01-08 12:00:05 +0300

    add server logic and project structure

---
+++ src/services/server.py
@@ -0,0 +1,49 @@
+import socket
+import threading
+
+class Server():
+    def __init__(self, PORT):
+        self.PORT = PORT if PORT else 5050
+        self.SERVER = socket.gethostbyname(socket.gethostname())
+        self.set_address(self.SERVER, self.PORT)
+
+        self.HEADER_LENGTH = 64
+        self.FORMAT = 'utf-8'
+        self.DISCONNECT_MESSAGE = "!DISCONNECT"
+
+        self.server = self.create_server(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.bind(self.ADDR)
+
+
+    def set_address(self, SERVER, PORT):
+        self.ADDR = (SERVER, PORT)
+
+    def create_server(self, address_family, socket_kind):
+        return socket.socket(address_family, socket_kind)
+    
+    def start(self):
+        print("[STARTING] Server is starting...")
+        self.server.listen()
+        print(f"[LISTENING] Server is listening on {self.SERVER}")
+        self.initiate_request_handler()
+
+    def initiate_request_handler(self):
+        while True:
+            conn, address = self.server.accept()
+            new_thread = threading.Thread(target=self.handle_client, args=(conn, address))
+            new_thread.start()
+
+    def handle_client(self, conn, address):
+        print(f"[NEW CONNECTION] {address} connected.")
+        is_alive = True
+        while is_alive:
+            message_leng = conn.recv(self.HEADER_LENGTH).decode(self.FORMAT)
+            message_length = int(message_leng)
+            message = conn.recv(message_length).decode(self.FORMAT)
+            if message == self.DISCONNECT_MESSAGE:
+                is_alive = False
+            print(f'[NEW MESSAGE] from {address} : {message}')
+        conn.close()
+
+    def count_active_users(self):
+        return threading.activeCount() - 1
